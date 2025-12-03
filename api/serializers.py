c64f8e8ba0fbc24a450d64dc3a064741c789061b
Jhonny Sampaio <jhonnysampaio622009@gmail.com>
2025-12-03 16:16:24 -0300

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,1 +1,17 @@
+from rest_framework import serializers
+from gerenciamento_escola.models import Aluno, Curso, Matricula
 
+class AlunoSerializer(serializer.ModelSerializer):
+  class Meta:
+    model = Aluno
+    fields = "__all__"
+
+class CursoSerializer(serializer.ModelSerializer):
+  class Meta:
+    model = Curso
+    fields = "__all__"
+
+class MatriculaSerializer(serializer.ModelSerializers):
+  class Meta:
+    model = Matricula
+    fields = "__all__"
