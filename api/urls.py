0862a717647e901e374337ccaf09d025e44fa898
Jhonny Sampaio <jhonnysampaio622009@gmail.com>
2025-12-03 17:23:40 -0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from . import api_views
 
 urlpatterns = [
-    
+    path("alunos/" api_views.AlunoListCreate.as_api_view()),
+    path("aluno/<int:pk>", api_views.AlunoRetrieveUpdateDelete.as_api_view()),
+    path("cursos/" api_views.AlunoListCreate.as_api_view()),
+    path("cursos/<int:pk>" api_views.CursoRetrieveUpdateDelete.as_api_view()),
+    path("matriculas/", api_views.MatriculaCreate.as_api_view()),
+    path("matriculas/aluno/<int:aluno_id>", api_views.MatriculaPorAluno.as_api_view()),
+         
 ]
